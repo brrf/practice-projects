e3ba5e46f617fb8c66dd112445ec3d7c7e24e42e
Moshe Praver <moshepraver@gmail.com>
2019-01-24 10:35:42 -0600

    destroyer function

--- intermediate-algorithm-scripting/intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting/intermediate-algorithm-scripting.js
@@ -10,4 +10,32 @@ function sumAll(arr) {
 	return sum;
 }
 
-console.log( sumAll([1, 4]) );
+function diffArray(arr1, arr2) {
+  var newArr = [];
+
+  function onlyInFirst (arra, arrb) {
+	  for (let i = 0; i < arra.length; i++) {
+	  	if (!arrb.includes(arra[i])) {
+	  		newArr.push(arra[i])
+	  	}
+	  }
+	}
+ 
+  onlyInFirst(arr1, arr2);
+  onlyInFirst(arr2, arr1);
+
+  return newArr;
+}
+
+function destroyer(arr, ...args) {
+	return arr.filter(function(number) {
+		return !args.includes(number)
+	});
+}
+
+console.log( destroyer([1, 2, 3, 1, 2, 3], 2, 3) );
+
+
+
+
+
