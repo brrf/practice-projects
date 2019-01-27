dee70f6bff70b7eb91911ddcab65b6c91ee64628
Moshe Praver <moshepraver@gmail.com>
2019-01-26 18:24:30 -0600

    completed the intermediate algo challenges

--- intermediate-algorithm-scripting/intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting/intermediate-algorithm-scripting.js
@@ -135,22 +135,6 @@ function convertHTML(str) {
  	return returnArray;
 }
 
-// function sumFibs(num) {
-// 	let fibs = [1, 1];
-// 	if (num === 1) return 1;
-// 	else if (num === 2) return 2;
-//  	else {
-//  		 while(( = arrFib[0] + arrFib[1]) <= num) {
-//         arrFib.unshift(nextFib);
-//     }
-// 		}
-// 	return fibs.filter( value => value % 2 === 1).reduce( (a,b) => a + b);
-// 	}
-// }
-
-
-// console.log(sumFibs(4000000));
-
 function sumFibs(num) {
 	let fibArray = [1, 1];
 	if (num === 1) return 1;
@@ -210,20 +194,19 @@ function smallestCommons(arr) {
 	  		}
 	  	}
 	  	return addMissingPrimes(remainder);
-	 }
+	}
 
-	 function addMissingPrimes (remainder) {
-	 	while (remainder > 1) {
-	  		for (let i = 2; i <= remainder; i++) {
-	  			if (remainder % i === 0) {
-	  				commonMultipleArr.push(i);
-	  				remainder /= i;
-	  				i--;
-
-	  			}
-	  		}
-	  	}
-	 }
+	function addMissingPrimes (remainder) {
+		while (remainder > 1) {
+			for (let i = 2; i <= remainder; i++) {
+				if (remainder % i === 0) {
+					commonMultipleArr.push(i);
+					remainder /= i;
+					i--;
+				}
+			}
+		}
+	}
 
 	for (let i = arr[0]; i <= arr[1]; i++) {
 		iterateExistingPrimes(i);
@@ -231,5 +214,106 @@ function smallestCommons(arr) {
 	 return commonMultipleArr.reduce( (a,b) => a * b );
 }
 
+function dropElements(arr, func) {
   
+	while (!func(arr[0])) {
+		arr.shift();
+	}
+	return arr;
+}
 
+function steamrollArray(arr) {
+	let returnArr = [];
+	
+	function flattenArrays (arr) {
+		for (let i = 0; i < arr.length; i++) {
+			if ( Array.isArray(arr[i]) ) {
+				flattenArrays(arr[i]);
+			} else {
+				returnArr.push(arr[i]);
+				console.log(returnArr);
+			}
+		}
+	}
+	flattenArrays(arr);
+	return returnArr;
+}
+
+function binaryAgent(str) {
+  let strArray = str.split(' ');
+  return strArray.map( item => parseInt(item, 2) ).map( item => String.fromCharCode(item) ).join(''); 
+}
+
+function truthCheck(collection, pre) {
+   for (let i = 0; i < collection.length; i++) {
+   	if (!collection[i][pre]) {
+   		return false;
+   	}
+   }
+  return true;
+}
+
+function addTogether(...args) {
+
+	function checkNum (num) {
+	  if (typeof num !== "number") {
+	  	return undefined;
+	  } else {
+	  	return num;
+	  }
+	}
+
+	if ( args.length === 1 && checkNum(args[0]) ) {
+		return function (num2) {
+			if (checkNum(num2)) {
+				return args[0] + num2;
+			}	
+		}
+	} else if ( args.length === 2 && checkNum(args[0]) && checkNum(args[1])) {
+		return args[0] + args[1];
+	} else return undefined;
+}
+
+var Person = function(firstAndLast) {
+
+  let fullName = firstAndLast;
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.getFirstName = function() {
+  	return fullName.split(' ')[0];
+  }
+
+  this.getLastName = function() {
+  	return fullName.split(' ')[1];
+  }
+
+  this.setFullName = function(name) {
+  	fullName = name;
+  }
+
+  this.setFirstName = function (firstName) {
+  	fullName = firstName + ' ' + fullName.split(' ')[1];
+  }
+
+  this.setLastName = function (lastName) {
+  	fullName = fullName.split(' ')[0] + ' ' + lastName;
+  }
+};
+
+function orbitalPeriod(arr) {
+  let returnArr = [];
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  
+  function calculateOrbitalPeriod (avgAlt) {
+  	return Math.round(2 * Math.PI * Math.sqrt( Math.pow(earthRadius + avgAlt, 3) / GM) );
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+  	returnArr.push({"name": arr[i]["name"], "orbitalPeriod": calculateOrbitalPeriod(arr[i]["avgAlt"])})
+  }
+  return returnArr;
+}
