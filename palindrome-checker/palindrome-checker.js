ae6aaf083c717678d692844cc29288ac8de479a8
Moshe Praver <moshepraver@gmail.com>
2019-01-27 15:03:09 -0600

    completed challenge

--- palindrome-checker/palindrome-checker.js
+++ palindrome-checker/palindrome-checker.js
@@ -1,1 +1,12 @@
-alert("je;;p world");
+"use strict"
+
+function palindrome(str) {
+  let strArray = str.toLowerCase().match(/[a-z0-9]/g);
+
+  for (let i = 0; i < strArray.length; i++) {
+  	if (strArray[i] !== strArray[strArray.length - 1 - i]) {
+  		return false;
+  	}
+  }
+  return true;
+}
