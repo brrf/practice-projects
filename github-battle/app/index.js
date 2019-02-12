2edb18d934cd6c035213eea6a1c348e957498057
Moshe Praver <moshepraver@gmail.com>
2019-02-12 17:35:15 -0600

    completed selectedLanguaged bar with highlight

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -3,43 +3,50 @@ var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 require('./index.css');
 
-class Users extends React.Component {
+class Languages extends React.Component {
+	constructor (props) {
+		super (props);
+			
+		this.state = {
+			selectedLanguage: 'All'
+		};
+
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+
+	
+	updateLanguage (lang) {
+		this.setState({
+			selectedLanguage: lang		
+		})
+	}
+
 	render () {
-		return (
-		     <div>
-		        <h1>Friends</h1>
-		        <ul>
-		          {this.props.list
-		          	.filter(user => user.friend)
-		          	.map(user => <li key={user.name}>{user.name}</li>)}
-		        </ul>
-		        
-		        <hr />
-		        
-		        <h1> Non Friends </h1>
-		        <ul>
-		            {this.props.list
-		          	.filter(user => !user.friend)
-		          	.map(user => <li key={user.name}>{user.name}</li>)}
-		        </ul>        
-	      </div>
-    	)
+
+	const languages= ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+
+	return (
+	  <ul className="languageList">
+	  	{languages.map( lang => 
+	  		<li 
+	  			style={lang === this.state.selectedLanguage ? {color: '#d0021b'} : null}
+	  			onClick={this.updateLanguage.bind(null, lang)}
+		  		key={lang}>
+		  			{lang}
+	  		</li>
+	  	)}
+	  </ul>
+	)
+
   	}
 }
 
-Users.propTypes = {
-	list: PropTypes.array.isRequired,
+// Languages.propTypes = {
+// 	list: PropTypes.array.isRequired,
 	
-}
+// }
 
-ReactDOM.render(<Users 
-					list={[
-					    { name: 'Tyler', friend: true },
-					    { name: 'Ryan', friend: true },
-					    { name: 'Michael', friend: false },
-					    { name: 'Mikenzi', friend: false },
-					    { name: 'Jessica', friend: true },
-					    { name: 'Dan', friend: false } 
-					]} />, 
+ReactDOM.render(<Languages />, 
 					document.getElementById('app')
 				)
