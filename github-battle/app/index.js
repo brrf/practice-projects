8e7d2d29886739438167c258b06375f765afcbc5
Moshe Praver <moshepraver@gmail.com>
2019-02-11 20:08:19 -0600

    added proptypes and divided user array into friends/notfriends

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,16 +1,45 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
 
-class App extends React.Component {
+class Users extends React.Component {
 	render () {
 		return (
-			<div>
-				Hello, Moshe Praver			
-			</div>
-		)
-	}
+		     <div>
+		        <h1>Friends</h1>
+		        <ul>
+		          {this.props.list
+		          	.filter(user => user.friend)
+		          	.map(user => <li key={user.name}>{user.name}</li>)}
+		        </ul>
+		        
+		        <hr />
+		        
+		        <h1> Non Friends </h1>
+		        <ul>
+		            {this.props.list
+		          	.filter(user => !user.friend)
+		          	.map(user => <li key={user.name}>{user.name}</li>)}
+		        </ul>        
+	      </div>
+    	)
+  	}
 }
 
+Users.propTypes = {
+	list: PropTypes.array.isRequired,
+	
+}
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<Users 
+					list={[
+					    { name: 'Tyler', friend: true },
+					    { name: 'Ryan', friend: true },
+					    { name: 'Michael', friend: false },
+					    { name: 'Mikenzi', friend: false },
+					    { name: 'Jessica', friend: true },
+					    { name: 'Dan', friend: false } 
+					]} />, 
+					document.getElementById('app')
+				)
