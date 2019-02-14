48195650d3ae78cfbcb0c5e5da89bebfba4aeb44
Moshe Praver <moshepraver@gmail.com>
2019-02-13 21:27:58 -0600

    adding routing and styled home page

--- github-battle/app/components/App.js
+++ github-battle/app/components/App.js
@@ -1,15 +1,24 @@
 var React = require('react');
-var Popular = require('./Popular')
+var ReactRouter = require('react-router-DOM');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+
+var Popular = require('./Popular');
+var Home = require('./Home')
 var Nav = require('./Nav')
 
 
 class App extends React.Component {
 	render () {
 		return (
-			<div>
-			 <Nav />
-		 	 <Popular />
-		 	</div>
+			<Router>
+				<div className='container'>
+					<Nav />
+					<Route exact path='/' component={Home} />
+					<Route path='/Popular' component={Popular} />
+				</div>
+			</Router>
 		)
   	}
 }
--- github-battle/app/components/Nav.js
+++ github-battle/app/components/Nav.js
@@ -1,49 +1,30 @@
 var React = require ('react');
+var NavLink = require('react-router-DOM').NavLink;
 
 
-function NavItems (props) {
-
-	const navItems = ['Home', 'Battle', 'Popular']
+function Nav () {
 
 	return (
 		<ul className='nav-list'>
-			{navItems.map(navItem => 
-				<li key={navItem}
-					onClick={props.onSelect.bind(null, navItem)}
-					style={navItem === props.navItem ? {fontWeight: 'bold'} : null}>
-					{navItem}
-				</li>
-			)}
+			<li>
+				<NavLink exact activeClassName='active' to='/'>
+					Home
+				</NavLink>
+			</li>
+
+			<li>
+				<NavLink activeClassName='active' to='/battle'>
+					Battle
+				</NavLink>
+			</li>
+
+			<li>
+				<NavLink activeClassName='active' to='/popular'>
+					Popular
+				</NavLink>
+			</li>
 		</ul>
 	)
-}
-
-
-
-
-class Nav extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {navItem: 'Home'};
-
-		this.updateNavItem = this.updateNavItem.bind(this);
-	};
-
-	updateNavItem (navItem) {
-		this.setState({
-			navItem: navItem,
-		})
-	}
-
-	
-	render() {
-		return (
-			<NavItems onSelect={this.updateNavItem}
-					  navItem={this.state.navItem}
-			/>
-		)
-	}
 }
 
 module.exports = Nav;
