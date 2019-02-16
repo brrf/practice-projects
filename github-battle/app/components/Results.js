6914e930ebc28d5c05b0bb3a20a2efdbc54f3a00
Moshe Praver <moshepraver@gmail.com>
2019-02-16 15:33:56 -0600

    parsed usernames from url and called github API with username.data

--- github-battle/app/components/Results.js
+++ github-battle/app/components/Results.js
@@ -1,11 +1,63 @@
 var React = require('React');
+var queryString = require('query-string');
+var api = require('../utils/api');
+var Link = require('react-router-dom').Link;
 
 class Results extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			winner: null,
+			loser: null,
+			error: null,
+			loading: true
+		}
+	}
+
+	componentDidMount () {
+		var players = queryString.parse(this.props.location.search);
+		api.battle([
+			players.playerOneName, 
+			players.playerTwoName])
+		.then(function(results) {
+			if (results === null) {
+				this.setState({
+					error: 'an error has occured!',
+					loading: false
+				})
+			}	
+
+			this.setState({
+				winner: results[0],
+				loser: results[1],
+				loading: false,
+			})
+		}.bind(this));
+	}
+
 	render () {
-		console.log(this.props)
+
+		let winner = this.state.winner;
+		let loser = this.state.loser;
+		console.log(winner)
+
+		if (this.state.loading === true) {
+			return <p>Loading...</p>
+		}
+
+		if (this.state.error) {
+			return (
+				<div>
+					<p>{this.state.error}</p>
+					<Link to='/battle'>Reset</Link>
+				</div>
+			)
+		}
+
 		return (
-			<div>
-				Results!
+			<div className='row'>
+				
 			</div>
 		)
 	}
