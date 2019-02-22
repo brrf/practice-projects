f0ca623687bfd804193a9b402e787826bd47189c
Moshe Praver <moshepraver@gmail.com>
2019-02-21 18:17:25 -0600

    added some middleware for debugging

--- learning-redux/index.js
+++ learning-redux/index.js
@@ -82,11 +82,21 @@ const checker = (store) => (next) => (action) => {
 	return next(action)
 }
 
+const logger = (store) => (next) => (action) => {
+	console.group(action.type)
+	console.log('The action: ', action);
+	const result = next(action);
+	console.log('The new state: ', store.getState())
+	console.groupEnd()
+
+	return result;
+}
+
 //initiate store
 const store = Redux.createStore(Redux.combineReducers({
 	todos,
 	goals
-}), Redux.applyMiddleware(checker));
+}), Redux.applyMiddleware(checker, logger));
 
 //Update DOM with state
 
