6524968a2b5433e05b6403c507c352c3173b1216
Moshe Praver <moshepraver@gmail.com>
2019-03-28 00:28:51 -0500

    refactored routes into separate routes folder

--- express-app/app.js
+++ express-app/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session')
-const { check, validationResult } = require('express-validator/check');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -69,97 +68,10 @@ app.get('/', (req, res) => {
 	})
 })
 
-//Get single article
-app.get('/article/:id', (req, res) => {
-	Article.findById(req.params.id, (err, article) => {
-		res.render('article', {
-			article: article
-		})
-	})
-})
+//Route files
 
-//Load edit form
-app.get('/article/edit/:id', (req, res) => {
-	Article.findById(req.params.id, (err, article) => {
-		res.render('edit_article', {
-			title: 'Edit Article',
-			article: article
-		})
-	})
-})
-
-//Add edited article
-app.post('/articles/edit/:id', (req, res) => {
-	let article = {};
-	article.title = req.body.title;
-	article.author = req.body.author;
-	article.body = req.body.body;
-
-	let query = {_id:req.params.id}
-
-	Article.update(query, article, err => {
-		if (err) {
-			console.log(err);
-			return;
-		} else {
-			req.flash('success', 'Article updated!')
-			res.redirect('/')
-		}
-
-	})
-})
-
-//Add Article Route
-
-app.get('/articles/add', (req, res) => {
-	res.render('add_article', {
-		title: 'Add Article'
-	})
-})
-
-//submit POST Route
-app.post('/articles/add', [
-	check('title', 'A title is required').not().isEmpty(),
-	check('author', 'An author is required').not().isEmpty(),
-	check('body', 'A body is required').not().isEmpty()	
-	], (req, res, next) => {
-
-	//Get errors
-	let errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return (res.render('add_article', {
-    		title: 'Add Article Working',
-    		errors: errors.mapped()
-    	}))
-	} else {
-		let article = new Article();
-		article.title = req.body.title;
-		article.author = req.body.author;
-		article.body = req.body.body;
-
-		article.save( (err) => {
-			if (err) {
-				console.log(err);
-				return;
-			} else {
-				req.flash('success', 'Article added!')
-				res.redirect('/')
-
-			}
-		})
-	}
-});
-
-//Delete article
-app.delete('/article/:id', (req, res) => {
-	let query = {_id:req.params.id}
-	Article.remove(query, (err) => {
-		if (err) {
-			console.log(err)
-		}
-		res.send('Success')
-	})
-})
+const articles = require('./routes/articles')
+app.use('/articles', articles)
 
 //Start server
 app.listen(3009, () => {
---
+++ express-app/routes/articles.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+//Bring in models
+let Article = require('../models/Article')
+
+
+
+
+//Load edit form
+router.get('/edit/:id', (req, res) => {
+	Article.findById(req.params.id, (err, article) => {
+		res.render('edit_article', {
+			title: 'Edit Article',
+			article: article
+		})
+	})
+})
+
+//Add edited article
+router.post('/edit/:id', (req, res) => {
+	let article = {};
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+
+	let query = {_id:req.params.id}
+
+	Article.update(query, article, err => {
+		if (err) {
+			console.log(err);
+			return;
+		} else {
+			req.flash('success', 'Article updated!')
+			res.redirect('/')
+		}
+
+	})
+})
+
+//Add Article Route
+
+router.get('/add', (req, res) => {
+	res.render('add_article', {
+		title: 'Add Article'
+	})
+})
+
+//submit POST Route
+router.post('/add', [
+	check('title', 'A title is required').not().isEmpty(),
+	check('author', 'An author is required').not().isEmpty(),
+	check('body', 'A body is required').not().isEmpty()	
+	], (req, res, next) => {
+
+	//Get errors
+	let errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return (res.render('add_article', {
+    		title: 'Add Article Working',
+    		errors: errors.mapped()
+    	}))
+	} else {
+		let article = new Article();
+		article.title = req.body.title;
+		article.author = req.body.author;
+		article.body = req.body.body;
+
+		article.save( (err) => {
+			if (err) {
+				console.log(err);
+				return;
+			} else {
+				req.flash('success', 'Article added!')
+				res.redirect('/')
+
+			}
+		})
+	}
+});
+
+//Delete article
+router.delete('/:id', (req, res) => {
+	let query = {_id:req.params.id}
+	Article.remove(query, (err) => {
+		if (err) {
+			console.log(err)
+		}
+		req.flash('danger', 'Article removed!')
+		res.send('Success')
+	})
+})
+
+//Get single article
+router.get('/:id', (req, res) => {
+	Article.findById(req.params.id, (err, article) => {
+		res.render('article', {
+			article: article
+		})
+	})
+})
+
+module.exports = router;
