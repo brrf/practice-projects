f5d66d7da8c0527884c4e60c0714d752e2e9fb4b
Moshe Praver <moshepraver@gmail.com>
2019-07-07 12:36:36 -0500

    handled asynchronous responses with promise chain

--- async exercises/ex3/ex3.js
+++ async exercises/ex3/ex3.js
@@ -20,8 +20,31 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	return new Promise( function(resolve, reject) {
+		fakeAjax(file, resolve)
+	})
 }
 
 // request all files at once in "parallel"
-// ???
+
+
+const file1 = getFile('file1');
+const file2 = getFile('file2');
+const file3 = getFile('file3');
+
+
+file1
+	.then( function(response1) {
+		output(response1)
+		return file2
+	})
+	.then(function(response2) {
+		output(response2)
+		return file3
+	})
+	.then(function(response3) {
+		output(response3)
+		console.log('done')
+	})
+
+
