5450da234502e14ece8ddd72c1183733159acbec
Moshe Praver <moshepraver@gmail.com>
2020-05-30 09:59:33 -0500

    added migrations for completion column

--- python_todo/app.py
+++ python_todo/app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://moshepraver@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
 	__tablename__ = 'todos'
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(), nullable=False)
+	completed = db.Column(db.Boolean, nullable=False, default=False)
 
 	def __repr__(self):
 		return f'<Todo {self.id} {self.description}'
-
-db.create_all()
 
 @app.route('/todos/create', methods=['post'])
 def create_todo():
@@ -23,7 +26,7 @@ def create_todo():
 	returnObject = {}
 	try: 
 		description = request.get_json()['description']
-		todo = Todo(description2=description)
+		todo = Todo(description=description)
 		db.session.add(todo)
 		db.session.commit()
 		returnObject['description'] = todo.description
