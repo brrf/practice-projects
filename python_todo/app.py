dac6b6936f1fb108ae9091367efaf96e84a5fc30
Moshe Praver <moshepraver@gmail.com>
2020-05-31 11:19:33 -0500

    Added crud to lists

--- python_todo/app.py
+++ python_todo/app.py
@@ -16,9 +16,17 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(), nullable=False)
 	completed = db.Column(db.Boolean, nullable=False, default=False)
+	list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
 	def __repr__(self):
 		return f'<Todo {self.id} {self.description}'
+
+class TodoList(db.Model):
+	__tablename__ = 'todolists'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(), nullable=False)
+	todos = db.relationship('Todo', backref='list', lazy=True)
+	completed = db.Column(db.Boolean, nullable=True, default=False)
 
 @app.route('/todos/create', methods=['post'])
 def create_todo():
@@ -26,7 +34,8 @@ def create_todo():
 	returnObject = {}
 	try: 
 		description = request.get_json()['description']
-		todo = Todo(description=description)
+		list_id = request.get_json()['list_id']
+		todo = Todo(description=description, list_id=list_id)
 		db.session.add(todo)
 		db.session.commit()
 		returnObject['description'] = todo.description
@@ -41,8 +50,29 @@ def create_todo():
 	else:
 		return returnObject
 
-@app.route('/todos/<todo_id>/togglecompleted', methods=['post'])
-def toggle_completed(todo_id):
+@app.route('/lists/create', methods=['post'])
+def create_list():
+	error = False
+	returnObject = {}
+	try:
+		name = request.get_json()['name']
+		list = TodoList(name=name)
+		db.session.add(list)
+		db.session.commit()
+		returnObject['name'] = list.name
+	except:
+		error = True
+		db.session.rollback()
+		print(sys.exc_info())
+	finally:
+		db.session.close()
+	if error:
+		return abort(400)
+	else:
+		return returnObject
+
+@app.route('/todos/<todo_id>/togglecompletedtodo', methods=['post'])
+def toggle_completed_todo(todo_id):
 	try:
 		completed = request.get_json()['completed']
 		todo = Todo.query.get(todo_id)
@@ -54,8 +84,70 @@ def toggle_completed(todo_id):
 		db.session.close()
 	return redirect(url_for('index'))
 
+@app.route('/lists/<list_id>/togglecompletedlist', methods=['post'])
+def toggle_completed_list(list_id):
+	try:
+		completed = request.get_json()['completed']
+		list = TodoList.query.get(list_id)
+
+		for todo in list.todos:
+			todo.completed = completed
+		list.completed = completed
+		db.session.commit()
+	except:
+		db.session.rollback()
+	finally:
+		db.session.close()
+	return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>/deletetodo', methods=['DELETE'])
+def delete_todo(todo_id):
+	error = False
+	try:
+		todo = Todo.query.get(todo_id)
+		db.session.delete(todo)
+		db.session.commit()
+	except:
+		db.session.rollback()
+		error = True
+	finally:
+		db.session.close()
+	if error:
+		abort(500)
+	else:
+		return jsonify({'success': True})
+
+@app.route('/lists/<list_id>/delete', methods=['DELETE'])
+def delete_list(list_id):
+    error = False
+    try:
+        list = TodoList.query.get(list_id)
+        for todo in list.todos:
+            db.session.delete(todo)
+
+        db.session.delete(list)
+        db.session.commit()
+    except():
+        db.session.rollback()
+        error = True
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify({'success': True})
+
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+	return render_template('index.html', 
+	lists=TodoList.query.order_by('id').all(),
+	todos=Todo.query.filter_by(list_id=list_id).order_by('id').all(),
+	active_list=TodoList.query.get(list_id)
+	)
 
 @app.route('/')
 def index():
-	return render_template('index.html', data=Todo.query.order_by('id').all())
+	return redirect(url_for('get_list_todos', list_id=1))
+
 
