413956e6b4d7ee12dffe451ebd3d277d8b4a6826
Moshe Praver <moshepraver@gmail.com>
2021-03-29 13:12:37 -0500

    practicing ng directives

--- angular-practice/basics-assignment-2-start/src/app/app.component.ts
+++ angular-practice/basics-assignment-2-start/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username: string = ''
+  username: string = '';
 
   isButtonDisabled (): boolean {
     return this.username === '';
