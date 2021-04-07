639bdbf1c3b51187e37c9a0233d08e8b78566994
Moshe Praver <moshepraver@gmail.com>
2021-04-07 13:28:31 -0500

    implemented add/delete shopping list

--- angular-first-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular-first-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,25 +1,62 @@
-import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', {static: false}) slForm: NgForm
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editMode = true;
+          this.editedItemIndex = index;
+          this.editedItem = this.shoppingListService.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+        }
+      )
   }
 
-  onAddItem() {
-    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
-    this.shoppingListService.addIngredient(newIngredient);
+  onClear() {
+    this.editMode = false;
+    this.slForm.reset()
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient)
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+    this.onClear()
+  }
+
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 
 }
--- angular-first-app/src/app/shopping-list/shopping-list.service.ts
+++ angular-first-app/src/app/shopping-list/shopping-list.service.ts
@@ -3,10 +3,19 @@ import { Subject } from 'rxjs';
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
         new Ingredient('Chicken', 4),
         new Ingredient('Broth', 2)
       ];
+
+    private notifySubjects() {
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
 
     getIngredients() {
         return this.ingredients.slice();
@@ -14,11 +23,21 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.next(this.ingredients.slice());
+        this.notifySubjects()
     }
 
     addIngredients(ingreidents: Ingredient[]) {
         this.ingredients.push(...ingreidents);
-        this.ingredientsChanged.next(this.ingredients.slice());
+        this.notifySubjects()
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient
+        this.notifySubjects()
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
+        this.notifySubjects();
     }
 }
