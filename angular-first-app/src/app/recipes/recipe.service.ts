7244c9bc5724c0ce7c148d9bc6f97069bada74af
Moshe Praver <moshepraver@gmail.com>
2021-04-11 18:28:36 -0500

    fetch/save data to firebase db

--- angular-first-app/src/app/recipes/recipe.service.ts
+++ angular-first-app/src/app/recipes/recipe.service.ts
@@ -7,32 +7,38 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>()
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Chicken soup',
-            'heals the soul! It cannot hurt',
-            'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_730,h_913/k%2Farchive%2F7ad08b34d013c250d1ec5a8293adf91c3a0d16c6',
-            [
-                new Ingredient('Chicken', 1),
-                new Ingredient('Broth', 3 )
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Chicken soup',
+    //         'heals the soul! It cannot hurt',
+    //         'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_730,h_913/k%2Farchive%2F7ad08b34d013c250d1ec5a8293adf91c3a0d16c6',
+    //         [
+    //             new Ingredient('Chicken', 1),
+    //             new Ingredient('Broth', 3 )
             
-            ]
-        ),
-        new Recipe(
-            'Broccoli Cheddar',
-            'had to!',
-            'https://www.tasteofhome.com/wp-content/uploads/2018/01/Cheese-Chicken-Soup_EXPS_BFBZ19_42311_E01_17_7b-2.jpg?fit=696,1024',
-            [
-                new Ingredient('Brocolli', 5),
-                new Ingredient('Cheese', 3)
-            ]
-        )
-    ];
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'Broccoli Cheddar',
+    //         'had to!',
+    //         'https://www.tasteofhome.com/wp-content/uploads/2018/01/Cheese-Chicken-Soup_EXPS_BFBZ19_42311_E01_17_7b-2.jpg?fit=696,1024',
+    //         [
+    //             new Ingredient('Brocolli', 5),
+    //             new Ingredient('Cheese', 3)
+    //         ]
+    //     )
+    // ];
+    private recipes: Recipe[] = []
 
     constructor(private shoppingListService: ShoppingListService) {}
 
     private notifySubjects() {
         this.recipesChanged.next(this.recipes.slice());
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.notifySubjects();
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
---
+++ angular-first-app/src/app/shared/data-storage.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+    
+  constructor(private http: HttpClient, private recipesService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http.put('https://angular-577ef-default-rtdb.firebaseio.com/recipes.json', recipes)
+        .subscribe(
+            response => console.log(response)
+        )
+  }
+
+  fetchRecipes() {
+      this.http
+        .get<Recipe[]>('https://angular-577ef-default-rtdb.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            })
+        }))
+        .subscribe(
+            recipes => {
+                console.log(recipes)
+                this.recipesService.setRecipes(recipes)
+            }
+        )
+  }
+}
