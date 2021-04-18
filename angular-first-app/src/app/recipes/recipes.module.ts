8f95e6abc5a2bdb1a45c9c58ca434a2ac970734d
Moshe Praver <moshepraver@gmail.com>
2021-04-18 18:43:40 -0500

    split the recipes routing

--- angular-first-app/src/app/recipes/recipes.module.ts
+++ angular-first-app/src/app/recipes/recipes.module.ts
@@ -5,9 +5,9 @@ import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RecipesRoutingModule } from './recipes-routing-module';
 
 @NgModule({
     declarations: [
@@ -27,7 +27,7 @@ import { ReactiveFormsModule } from '@angular/forms';
         EditRecipeComponent,
     ],
     imports: [
-        RouterModule,
+        RecipesRoutingModule,
         CommonModule,
         ReactiveFormsModule
     ]
