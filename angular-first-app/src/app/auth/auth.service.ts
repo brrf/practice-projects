e332e2510371aa7cd2b443cff4491d5f68285390
Moshe Praver <moshepraver@gmail.com>
2021-04-20 12:39:47 -0500

    used env variables for api key

--- angular-first-app/src/app/auth/auth.service.ts
+++ angular-first-app/src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment'
 
 
 export interface AuthResponseData {
@@ -24,7 +25,7 @@ export class AuthService {
     constructor(private http: HttpClient, private router: Router) {}
 
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCowVz5HRBLT4Vjug0u8fKpNFiKE25q6CM',
+        return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
             {
                 email,
                 password,
@@ -40,7 +41,7 @@ export class AuthService {
     };
 
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCowVz5HRBLT4Vjug0u8fKpNFiKE25q6CM',
+        return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
             {
                 email,
                 password,
--- angular-first-app/src/environments/environment.prod.ts
+++ angular-first-app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyCowVz5HRBLT4Vjug0u8fKpNFiKE25q6CM'
 };
